a2f47d43409958c4952e69aeb7ca3f2082c5aff7
Robertas Maleckas <maleckas.robertas@gmail.com>
2019-09-30 20:17:05 +0200

    Scaffold user-facing web server ASP.NET Core project

---
+++ WebServer/Controllers/CertController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebServer.Models;
+
+namespace WebServer.Controllers
+{
+    public class CertController : Controller
+    {
+        private readonly ILogger<CertController> _logger;
+
+        public CertController(ILogger<CertController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // TODO: Enable XSRF protection for HttpPost endpoints if not present by default
+        // TODO: Rate limit
+        [HttpPost]
+        public IActionResult New()
+        {
+            throw new NotImplementedException();
+        }
+
+        // TODO: consider replacing string argument with a model object
+        [HttpPost]
+        public IActionResult Revoke(string serialNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebServer/Startup.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WebServer
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            // TODO: Configure cookie & cert auth options, create auth policies
+            services.AddAuthentication()
+                .AddCookie()
+                .AddCertificate();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // TODO: consider adding a user friendly exception handler page
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Cert}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
