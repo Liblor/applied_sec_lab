c7f9a114cac207f2cb740a95a07a1a11041abe95
Robertas Maleckas <rmaleckas@student.ethz.ch>
2019-10-02 19:34:13 +0200

    Implement email + password auth stubs

--- WebServer/Controllers/AccountController.cs
+++ WebServer/Controllers/AccountController.cs
@@ -1,21 +1,81 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
 using WebServer.Models;
+using WebServer.Models.Account;
 
 namespace WebServer.Controllers
 {
 	public class AccountController : Controller
 	{
-		[HttpGet]
+		[HttpGet, Authorize]
 		public IActionResult Index()
 		{
 			return View();
 		}
 
-		[HttpPost]
+		[HttpGet, AllowAnonymous]
+		public IActionResult Login()
+		{
+			return View();
+		}
+
+		[HttpPost, AllowAnonymous]
+		public async Task<IActionResult> Login(LoginDetails loginDetails, string returnUrl = null)
+		{
+			if (ModelState.IsValid)
+			{
+				// TODO: validate credentials against DB 
+				// This should be constructed using info from the DB
+				var user = new User
+				{
+					Email = loginDetails.Email,
+					FirstName = "Test",
+					LastName = "User",
+					Id = "testuser"
+				};
+
+				var claims = new List<Claim>
+				{
+					new Claim(ClaimTypes.NameIdentifier, user.Id),
+					new Claim(ClaimTypes.Name, user.Name),
+					new Claim(ClaimTypes.Email, user.Email)
+				};
+
+				var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+				var authProps = new AuthenticationProperties
+				{
+					// TODO: configure
+				};
+
+				await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(identity), authProps);
+
+				// TODO: assess potential open redirect vulnerability
+				if (!string.IsNullOrWhiteSpace(returnUrl))
+					return Redirect(returnUrl);
+
+				return RedirectToAction(nameof(Index));
+			}
+
+			return View(loginDetails);
+		}
+
+		[HttpPost, Authorize]
 		public IActionResult Update(User user)
 		{
 			throw new NotImplementedException();
+		}
+
+		[HttpPost, Authorize]
+		public async Task<IActionResult> Logout()
+		{
+			await HttpContext.SignOutAsync();
+			return RedirectToAction(nameof(Login));
 		}
 	}
 }
