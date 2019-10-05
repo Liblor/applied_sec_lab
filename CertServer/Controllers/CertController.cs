24c3dc2f57d11fcd291a4956ae17d4c4a694fcc1
Loris Reiff <loris.reiff@liblor.ch>
2019-10-05 19:19:09 +0200

    Skeletton for CertServer

---
+++ CertServer/Controllers/CertController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Net;
+using System.Web;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CertServer.Controllers
+{
+
+	//[ApiController]
+	[Route("api")]
+	public class CertController : ControllerBase
+	{
+		[HttpGet]
+		public Dictionary<string, string> Get()
+		{
+			// XXX: Hacky (lots of love, C# noobs)
+			return new Dictionary<string, string>
+				{
+					{"issue", "/api/issue"},
+					{"revoke", "/api/revoke"},
+					{"ciphersuites", "/api/ciphersuites"},
+				};
+		}
+	}
+}
