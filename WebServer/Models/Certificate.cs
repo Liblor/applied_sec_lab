bfdf188785f54a043e323c2e1223ab4e31ffe169
eikendev <raphael@eiken.dev>
2019-10-05 21:21:22 +0200

    Add button to revoke certificates.

--- WebServer/Models/Certificate.cs
+++ WebServer/Models/Certificate.cs
@@ -4,6 +4,7 @@ namespace WebServer.Models
 {
     public class Certificate
     {
+        public int Id { get; set; }
         public string Fingerprint { get; set; }
         public DateTime CreateDate { get; set; }
         public DateTime ExpireDate { get; set; }
