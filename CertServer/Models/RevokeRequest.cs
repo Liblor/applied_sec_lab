37166b08c88fe9bf5efe6b3273acc21ebc110f92
Miro-H <miro.haller@gmail.com>
2019-10-14 19:36:22 +0200

    Add structure for certificate revocation

---
+++ CertServer/Models/RevokeRequest.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CertServer.Models
+{
+    public class RevokeRequest
+    {
+        [Required]
+        public string Uid { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public int SerialNumber { get; set; }
+    }
+}
