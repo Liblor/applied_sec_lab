6ce64cc28053677b21ce36a22d86de29ab2a75a2
Miro-H <miro.haller@gmail.com>
2019-10-20 00:11:48 +0200

    Protect issued private key with user provided passphrase. Combine certificate and private key in a single pkcs12 file.

--- CertServer/Models/UserCertificate.cs
+++ CertServer/Models/UserCertificate.cs
@@ -2,7 +2,6 @@ namespace CertServer.Models
 {
     public class UserCertificate
     {
-        public string PrivateKey { get; set; }
-        public string Certificate { get; set; }
+        public string Pkcs12Archive { get; set; }
     }
 }
