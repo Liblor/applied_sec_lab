1291502364bc97e281a9b7e9f6a2cdcada6ee1c5
Loris Reiff <loris.reiff@liblor.ch>
2019-10-25 00:33:48 +0200

    MariaDB + Galera replicate
    Initial configuration for galera
    
    Todo: create db, tables, fix permissions, ssl cert for galera

---
+++ vagrant_share/ansible/roles/asl-dbserver/files/public_certs.sql
@@ -0,0 +1,15 @@
+CREATE TABLE public_certificates (
+    serial_number INT NOT NULL,
+    uid VARCHAR(64) NOT NULL DEFAULT '',
+    public_cert TEXT NOT NULL,
+    revoked BOOLEAN DEFAULT FALSE,
+    PRIMARY KEY(serial_number)
+); 
+
+
+CREATE TABLE private_certificates (
+    sk_id INT NOT NULL AUTO_INCREMENT UNIQUE,
+    uid varchar(64) NOT NULL DEFAULT '' UNIQUE,
+    private_cert TEXT,
+    PRIMARY KEY(uid)
+); 
