1845b41ae96ab7c95dc5513a0fdee4f83bdc8c4c
Miro-H <miro.haller@gmail.com>
2019-11-10 00:16:21 +0100

    Add our root certificate to browser's root of trust

---
+++ vagrant_share/scripts/mozilla-import-certificates.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+### Script installs root.cert.pem to certificate trust store of applications using NSS
+### (e.g. Firefox, Thunderbird, Chromium)
+### Mozilla uses cert8, Chromium and Chrome use cert9
+### Source: https://thomas-leister.de/en/how-to-import-ca-root-certificate/
+certfile="/etc/ssl/certs/iMovies_Root_CA.pem"
+certname="iMovies Root CA"
+homefolder="$1"
+
+###
+### For cert8 (legacy - DBM)
+###
+
+for certDB in $(find $homefolder -name "cert8.db")
+do
+    echo $certDB
+    certdir=$(dirname ${certDB});
+    certutil -A -n "${certname}" -t "TCu,Cu,Tu" -i ${certfile} -d dbm:${certdir}
+done
+
+
+###
+### For cert9 (SQL)
+###
+
+for certDB in $(find $homefolder -name "cert9.db")
+do
+    echo $certDB
+    certdir=$(dirname ${certDB});
+    certutil -A -n "${certname}" -t "TCu,Cu,Tu" -i ${certfile} -d sql:${certdir}
+done
