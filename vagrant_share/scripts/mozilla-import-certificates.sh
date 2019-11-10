92641901cc7a321b82cfe108b007849802f356b4
Miro-H <miro.haller@gmail.com>
2019-11-10 11:13:20 +0100

    Not add certificate to OS root of trust. Enable autologin.

--- vagrant_share/scripts/mozilla-import-certificates.sh
+++ vagrant_share/scripts/mozilla-import-certificates.sh
@@ -4,7 +4,7 @@
 ### (e.g. Firefox, Thunderbird, Chromium)
 ### Mozilla uses cert8, Chromium and Chrome use cert9
 ### Source: https://thomas-leister.de/en/how-to-import-ca-root-certificate/
-certfile="/etc/ssl/certs/iMovies_Root_CA.pem"
+certfile="/vagrant/key_store/iMovies_Root_CA.crt"
 certname="iMovies Root CA"
 homefolder="$1"
 
@@ -14,7 +14,7 @@ homefolder="$1"
 
 for certDB in $(find $homefolder -name "cert8.db")
 do
-    echo $certDB
+    echo "Add certificate '${certname}' to $certDB"
     certdir=$(dirname ${certDB});
     certutil -A -n "${certname}" -t "TCu,Cu,Tu" -i ${certfile} -d dbm:${certdir}
 done
@@ -26,7 +26,7 @@ done
 
 for certDB in $(find $homefolder -name "cert9.db")
 do
-    echo $certDB
+    echo "Add certificate '${certname}' to $certDB"
     certdir=$(dirname ${certDB});
     certutil -A -n "${certname}" -t "TCu,Cu,Tu" -i ${certfile} -d sql:${certdir}
 done
