73f70e74317c9be2e12e6dad1efb94f44f531244
Miro-H <miro.haller@gmail.com>
2019-11-11 21:14:42 +0100

    Add shared min length for passwords and passphrases. Adapt test cases. Closes #43

--- CertServer/Models/PrivateKey.cs
+++ CertServer/Models/PrivateKey.cs
@@ -1,13 +1,15 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
+using CoreCA.DataModel;
+
 namespace CertServer.Models
 {
     public class PrivateKey
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
         [Column("uid")]
-        [StringLength(64, MinimumLength = 1)]
+        [StringLength(64, MinimumLength = Constants.MinPasswordLength)]
         public string Uid { get; set; }
 
         [Column("private_cert", TypeName = "ntext")]
