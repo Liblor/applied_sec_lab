7a4461f6379faa6d0f6fb5a081f9136ab70995ce
Miro-H <miro.haller@gmail.com>
2019-11-14 00:36:34 +0100

    Fix bugs on first ansible run, improvements. Encrypted file is created. TODO: Testing and push to backup.

--- CertServer/CAConfig.cs
+++ CertServer/CAConfig.cs
@@ -13,7 +13,7 @@ namespace CertServer
         public static readonly string CoreCACertPath = "/home/coreca/pki/private/iMovies_external_"
             + System.Environment.MachineName
             + "_Intermediate_CA_cert_and_priv_key.pfx";
-        public static readonly string BackupServerCertPath = "/etc/pki/bkp/certs/iMovies_bkp_pub.crt";
+        public static readonly string BackupServerCertPath = "/etc/pki/bkp/certs/iMovies_bkp.crt";
         public static readonly string BackupFolder = "/home/coreca/backup/";
 
         public static readonly int UserCertValidityPeriod = 150;
